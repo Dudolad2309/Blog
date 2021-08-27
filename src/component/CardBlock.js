dabb6e4d2cf0b725d1efb5eb62f46a4896df2a79
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:59:48 +0300

    refactor

--- src/component/CardBlock.js
+++ src/component/CardBlock.js
@@ -5,59 +5,48 @@ import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    cardMedia: {
-        paddingTop: "56%",
+	cardMedia: {
+		paddingTop: "56%",
+	},
+	cardContent: {
+		flexGrow: 1
+	},
+}));
 
-    },
-    cardContent: {
-        flexGrow: 1
-    },
+export const CardBlock = (props) => {
+	const classes = useStyles();
 
-}))
-
- export const CardBlock = (props) => {
-     const classes = useStyles()
-
-
-
-
-
-    return (
-        <Grid container spacing={4}>
-
-            {props.cards.map((card) => (
-
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Card className={classes.card}>
-                        <CardMedia className={classes.cardMedia}
-                                   image="https://picsum.photos/640/360"
-                                   title="Image title"
-                        />
-                        <CardContent className={classes.cardContent}>
-                            <Typography variant="h5" gutterBottom>
-                                Blog post
-                            </Typography>
-                            <Typography>
-                                Использование этого подхода в продакшене не рекомендуется
-                                - клиент вынужден загружать
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                View
-                            </Button>
-                            <Button size="small" color="primary">
-                                Edit
-                            </Button>
-                            <ContactlessOutlinedIcon color="secondary"/>
-                            <AccessAlarmIcon color="primary"/>
-                        </CardActions>
-                    </Card>
-                </Grid>
-
-            ))}
-
-        </Grid>
-
-    )
+	return (
+		<Grid container spacing={4}>
+			{props.cards.map((card) => (
+				<Grid item key={card} xs={12} sm={6} md={4}>
+					<Card className={classes.card}>
+						<CardMedia className={classes.cardMedia}
+						           image="https://picsum.photos/640/360"
+						           title="Image title"
+						/>
+						<CardContent className={classes.cardContent}>
+							<Typography variant="h5" gutterBottom>
+								Blog post
+							</Typography>
+							<Typography>
+								Использование этого подхода в продакшене не рекомендуется
+								- клиент вынужден загружать
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small" color="primary">
+								View
+							</Button>
+							<Button size="small" color="primary">
+								Edit
+							</Button>
+							<ContactlessOutlinedIcon color="secondary"/>
+							<AccessAlarmIcon color="primary"/>
+						</CardActions>
+					</Card>
+				</Grid>
+			))}
+		</Grid>
+	)
 }
